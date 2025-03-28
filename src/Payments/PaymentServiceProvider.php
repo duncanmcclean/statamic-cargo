4370fca2f4a4bac3d924d1b41d442fb2ab0ef0ee
Duncan McClean <duncan@duncanmcclean.com>
2025-03-28 12:06:49 +0000

    wip

--- src/Payments/PaymentServiceProvider.php
+++ src/Payments/PaymentServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace DuncanMcClean\Cargo\Payments;
 
+use DuncanMcClean\Cargo\Payments\Gateways\PaymentGateway;
+use DuncanMcClean\Cargo\Shipping\ShippingMethod;
 use Statamic\Providers\AddonServiceProvider;
 
 class PaymentServiceProvider extends AddonServiceProvider
@@ -16,6 +18,26 @@ class PaymentServiceProvider extends AddonServiceProvider
     {
         foreach ($this->paymentGateways as $paymentGateway) {
             $paymentGateway::register();
+        }
+
+        $this->registerAppExtensions('PaymentGateways', PaymentGateway::class);
+    }
+
+    protected function registerAppExtensions($folder, $requiredClass)
+    {
+        if (! $this->app['files']->exists($path = app_path($folder))) {
+            return;
+        }
+
+        foreach ($this->app['files']->allFiles($path) as $file) {
+            $relativePathOfFolder = str_replace(app_path(DIRECTORY_SEPARATOR), '', $file->getPath());
+            $namespace = str_replace('/', '\\', $relativePathOfFolder);
+            $class = $file->getBasename('.php');
+
+            $fqcn = $this->app->getNamespace()."{$namespace}\\{$class}";
+            if (is_subclass_of($fqcn, $requiredClass)) {
+                $fqcn::register();
+            }
         }
     }
 }
--- src/Shipping/ShippingServiceProvider.php
+++ src/Shipping/ShippingServiceProvider.php
@@ -15,5 +15,25 @@ class ShippingServiceProvider extends AddonServiceProvider
         foreach ($this->shippingMethods as $shippingMethod) {
             $shippingMethod::register();
         }
+
+        $this->registerAppExtensions('ShippingMethods', ShippingMethod::class);
+    }
+
+    protected function registerAppExtensions($folder, $requiredClass)
+    {
+        if (! $this->app['files']->exists($path = app_path($folder))) {
+            return;
+        }
+
+        foreach ($this->app['files']->allFiles($path) as $file) {
+            $relativePathOfFolder = str_replace(app_path(DIRECTORY_SEPARATOR), '', $file->getPath());
+            $namespace = str_replace('/', '\\', $relativePathOfFolder);
+            $class = $file->getBasename('.php');
+
+            $fqcn = $this->app->getNamespace()."{$namespace}\\{$class}";
+            if (is_subclass_of($fqcn, $requiredClass)) {
+                $fqcn::register();
+            }
+        }
     }
 }
