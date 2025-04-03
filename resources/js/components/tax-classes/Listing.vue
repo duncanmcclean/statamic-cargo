d96c843f096eae03274b294640929ecb197cbb4d
Duncan McClean <duncan@duncanmcclean.com>
2025-04-03 23:41:12 +0100

    listings

--- resources/js/components/tax-classes/Listing.vue
+++ resources/js/components/tax-classes/Listing.vue
@@ -1,11 +1,14 @@
 <template>
     <data-list :rows="rows" :columns="columns">
-        <div class="card p-0" slot-scope="{ }">
+        <div class="card p-0">
             <data-list-table>
-                <template slot="cell-name" slot-scope="{ row: taxClass, index }">
+                <template #cell-name="{ row: taxClass, index }">
                     <a :href="taxClass.edit_url">{{ __(taxClass.name) }}</a>
                 </template>
-                <template slot="actions" slot-scope="{ row: taxClass, index }">
+                <template #cell-handle="{ value: handle }">
+                    <span class="font-mono text-xs">{{ handle }}</span>
+                </template>
+                <template #actions="{ row: taxClass, index }">
                     <dropdown-list>
                         <dropdown-item :text="__('Edit')" :redirect="taxClass.edit_url" />
                         <dropdown-item
@@ -30,7 +33,6 @@
 import Listing from '@statamic/components/Listing.vue'
 
 export default {
-
     mixins: [Listing],
 
     props: [
@@ -44,6 +46,5 @@ export default {
             columns: this.initialColumns
         }
     }
-
 }
 </script>
