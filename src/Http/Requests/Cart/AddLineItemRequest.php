ecb5f075ec9c887364e8f881b1af17331cddb2cb
Duncan McClean <duncan@duncanmcclean.com>
2025-04-24 23:32:40 +0100

    Use `Localize` trait on form requests

--- src/Http/Requests/Cart/AddLineItemRequest.php
+++ src/Http/Requests/Cart/AddLineItemRequest.php
@@ -5,11 +5,13 @@ namespace DuncanMcClean\Cargo\Http\Requests\Cart;
 use DuncanMcClean\Cargo\Facades\Product;
 use DuncanMcClean\Cargo\Http\Requests\Concerns\AcceptsCustomFormRequests;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Traits\Localizable;
 use Illuminate\Validation\Rule;
+use Statamic\Facades\Site;
 
 class AddLineItemRequest extends FormRequest
 {
-    use AcceptsCustomFormRequests;
+    use AcceptsCustomFormRequests, Localizable;
 
     public function authorize()
     {
@@ -58,5 +60,22 @@ class AddLineItemRequest extends FormRequest
             ],
             'quantity' => ['nullable', 'integer', 'gt:0'],
         ];
+    }
+
+    public function validateResolved()
+    {
+        // If this was submitted from a front-end form, we want to use the appropriate language
+        // for the translation messages. If there's no previous url, it was likely submitted
+        // directly in a headless format. In that case, we'll just use the default lang.
+        $site = ($previousUrl = $this->previousUrl()) ? Site::findByUrl($previousUrl) : null;
+
+        return $this->withLocale($site?->lang(), fn () => parent::validateResolved());
+    }
+
+    private function previousUrl()
+    {
+        return ($referrer = request()->header('referer'))
+            ? url()->to($referrer)
+            : session()->previousUrl();
     }
 }
--- src/Http/Requests/Cart/UpdateLineItemRequest.php
+++ src/Http/Requests/Cart/UpdateLineItemRequest.php
@@ -5,11 +5,13 @@ namespace DuncanMcClean\Cargo\Http\Requests\Cart;
 use DuncanMcClean\Cargo\Facades\Cart;
 use DuncanMcClean\Cargo\Http\Requests\Concerns\AcceptsCustomFormRequests;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Traits\Localizable;
 use Statamic\Exceptions\NotFoundHttpException;
+use Statamic\Facades\Site;
 
 class UpdateLineItemRequest extends FormRequest
 {
-    use AcceptsCustomFormRequests;
+    use AcceptsCustomFormRequests, Localizable;
 
     public function authorize()
     {
@@ -32,5 +34,22 @@ class UpdateLineItemRequest extends FormRequest
             'variant' => ['nullable'],
             'quantity' => ['nullable', 'integer', 'gt:0'],
         ];
+    }
+
+    public function validateResolved()
+    {
+        // If this was submitted from a front-end form, we want to use the appropriate language
+        // for the translation messages. If there's no previous url, it was likely submitted
+        // directly in a headless format. In that case, we'll just use the default lang.
+        $site = ($previousUrl = $this->previousUrl()) ? Site::findByUrl($previousUrl) : null;
+
+        return $this->withLocale($site?->lang(), fn () => parent::validateResolved());
+    }
+
+    private function previousUrl()
+    {
+        return ($referrer = request()->header('referer'))
+            ? url()->to($referrer)
+            : session()->previousUrl();
     }
 }
