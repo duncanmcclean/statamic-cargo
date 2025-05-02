a7f25b4edc1691a34cbd81041e41081f76c8becc
Duncan McClean <duncan@duncanmcclean.com>
2025-05-02 15:24:57 +0100

    Don't bring in our `.css` file until we upgrade to Tailwind CSS 4

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -57,7 +57,7 @@ class ServiceProvider extends AddonServiceProvider
         'publicDirectory' => 'resources/dist',
         'input' => [
             'resources/js/cp.js',
-            'resources/css/cp.css',
+//            'resources/css/cp.css',
         ],
     ];
 
