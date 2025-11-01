13521784f9af1c025b3e275887c3bca36e221e2d
Duncan McClean <duncan@duncanmcclean.com>
2025-11-01 18:37:09 +0000

    Simplify tax rates fieldtype (#66)

--- src/Fieldtypes/TaxRates.php
+++ src/Fieldtypes/TaxRates.php
@@ -4,26 +4,13 @@ namespace DuncanMcClean\Cargo\Fieldtypes;
 
 use DuncanMcClean\Cargo\Facades\TaxClass;
 use Statamic\Fields\Fields;
-use Statamic\Fields\Fieldtype;
-use Statamic\Support\Arr;
+use Statamic\Fieldtypes\Group as GroupFieldtype;
 
-class TaxRates extends Fieldtype
+class TaxRates extends GroupFieldtype
 {
     protected $selectable = false;
 
-    public function process($data)
-    {
-        $values = $this->fields()->addValues($data ?? [])->process()->values()->all();
-
-        return Arr::removeNullValues($values);
-    }
-
-    public function preProcess($data)
-    {
-        return $this->fields()->addValues($data ?? [])->preProcess()->values()->all();
-    }
-
-    private function fields(): Fields
+    public function fields(): Fields
     {
         $fields = TaxClass::all()->map(fn ($taxClass) => [
             'handle' => $taxClass->handle(),
@@ -39,58 +26,11 @@ class TaxRates extends Fieldtype
         return new Fields($fields, $this->field()->parent(), $this->field());
     }
 
-    public function rules(): array
-    {
-        return ['array'];
-    }
-
-    public function extraRules(): array
-    {
-        $rules = $this
-            ->fields()
-            ->addValues((array) $this->field->value())
-            ->validator()
-            ->withContext([
-                'prefix' => $this->field->validationContext('prefix'),
-            ])
-            ->rules();
-
-        return collect($rules)->mapWithKeys(function ($rules, $handle) {
-            return [$this->field->handle().'.'.$handle => $rules];
-        })->all();
-    }
-
-    public function extraValidationAttributes(): array
-    {
-        return collect($this->fields()->validator()->attributes())->mapWithKeys(function ($attribute, $handle) {
-            return [$this->field->handle().'.'.$handle => $attribute];
-        })->all();
-    }
-
     public function preload()
     {
         return [
             'fields' => $this->fields()->all(),
             'meta' => $this->fields()->addValues($this->field->value() ?? $this->defaultGroupData())->meta()->toArray(),
         ];
-    }
-
-    protected function defaultGroupData()
-    {
-        return $this->fields()->all()->map(function ($field) {
-            return $field->fieldtype()->preProcess($field->defaultValue());
-        })->all();
-    }
-
-    public function preProcessValidatable($value)
-    {
-        return array_merge(
-            $value ?? [],
-            $this->fields()
-                ->addValues($value ?? [])
-                ->preProcessValidatables()
-                ->values()
-                ->all(),
-        );
     }
 }
