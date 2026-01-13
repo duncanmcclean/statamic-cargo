c49ad29c30b0ca432ab3988831335b8ef36ad604
Duncan McClean <duncan@duncanmcclean.com>
2026-01-13 20:22:35 +0000

    Move methods into `Productable` trait...
    We're going to need to Product classes:
    
    - One that extends the core Entry class
    - One that extends the Eloquent Driver's Entry class
    
    The `Productable` trait will be used by both of them.

--- src/Contracts/Products/Product.php
+++ src/Contracts/Products/Product.php
@@ -2,11 +2,24 @@
 
 namespace DuncanMcClean\Cargo\Contracts\Products;
 
-/**
- * @mixin \Statamic\Entries\Entry
- * @mixin \DuncanMcClean\Cargo\Products\Product
- */
+use DuncanMcClean\Cargo\Products\ProductVariant;
+use Illuminate\Support\Collection;
+
 interface Product
 {
-    //
+    public function isStandardProduct(): bool;
+
+    public function isVariantProduct(): bool;
+
+    public function price(): ?int;
+
+    public function productVariants(): array;
+
+    public function stock(): ?int;
+
+    public function isStockEnabled(): bool;
+
+    public function variantOptions(): Collection;
+
+    public function variant(string $optionKey): ?ProductVariant;
 }
