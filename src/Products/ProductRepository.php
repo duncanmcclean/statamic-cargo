b269fccc5fe8d94ca830a43c23806986768d9a9d
Duncan McClean <duncan@duncanmcclean.com>
2026-01-13 20:08:57 +0000

    Drop `Product::fromEntry()` method

--- src/Products/ProductRepository.php
+++ src/Products/ProductRepository.php
@@ -22,22 +22,15 @@ class ProductRepository implements RepositoryContract
     {
         return Entry::query()
             ->whereIn('collection', $this->collections)
-            ->get()
-            ->map(fn ($entry) => $this->fromEntry($entry));
+            ->get();
     }
 
     public function find($id): ?Product
     {
-        $entry = Entry::query()
+        return Entry::query()
             ->whereIn('collection', $this->collections)
             ->where('id', $id)
             ->find($id);
-
-        if (! $entry) {
-            return null;
-        }
-
-        return $this->fromEntry($entry);
     }
 
     public function findOrFail($id): Product
@@ -46,33 +39,6 @@ class ProductRepository implements RepositoryContract
 
         if (! $product) {
             throw new ProductNotFound("Product [{$id}] could not be found.");
-        }
-
-        return $product;
-    }
-
-    public function fromEntry(EntryContract $entry): Product
-    {
-        $product = app(Product::class)
-            ->id($entry->id())
-            ->collection($entry->collection());
-
-        if ($origin = $entry->origin()) {
-            $product->origin($origin);
-        }
-
-        $product
-            ->blueprint($entry->blueprint())
-            ->locale($entry->locale())
-            ->initialPath($entry->initialPath())
-            ->published($entry->published())
-            ->data($entry->data())
-            ->slug($entry->slug())
-            ->template($entry->template())
-            ->layout($entry->layout());
-
-        if ($entry->hasDate()) {
-            $product->date($entry->date());
         }
 
         return $product;
