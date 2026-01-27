b12bb59a3b0d8c8f0858815eab610ce3fce58b52
Duncan McClean <duncan@duncanmcclean.com>
2026-01-27 13:55:36 +0000

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Str;
 use PHPUnit\Framework\Assert;
 use ReflectionClass;
 use Statamic\Facades\Config;
+use Statamic\Facades\Path;
 use Statamic\Facades\Site;
 use Statamic\Testing\AddonTestCase;
 use Statamic\Testing\Concerns\PreventsSavingStacheItemsToDisk;
@@ -27,13 +28,8 @@ abstract class TestCase extends AddonTestCase
         $uses = array_flip(class_uses_recursive(static::class));
 
         if (isset($uses[PreventsSavingStacheItemsToDisk::class])) {
-            // Old
             $reflection = new ReflectionClass($this);
-            $this->fakeStacheDirectory = Str::before(dirname($reflection->getFileName()), DIRECTORY_SEPARATOR.'tests').'/tests/__fixtures__/dev-null';
-
-            // New
-            //            $reflector = new ReflectionClass($this->addonServiceProvider);
-            //            $this->fakeStacheDirectory = dirname($reflector->getFileName()).'/../tests/__fixtures__/dev-null';
+            $this->fakeStacheDirectory = Path::tidy(Str::before(dirname($reflection->getFileName()), DIRECTORY_SEPARATOR.'tests').'/tests/__fixtures__/dev-null');
 
             $this->preventSavingStacheItemsToDisk();
         }
