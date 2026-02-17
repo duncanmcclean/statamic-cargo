8a4177feb9fb64d794267a1b781bacd88fc5cac0
Duncan McClean <duncan@duncanmcclean.com>
2026-02-17 22:20:30 +0000

    `Cart::order()` doesn't exist. Need to query orders.

--- src/Payments/Gateways/Mollie.php
+++ src/Payments/Gateways/Mollie.php
@@ -148,10 +148,11 @@ class Mollie extends PaymentGateway
     public function cancel(Cart $cart): void
     {
         $payment = $this->mollie->payments->get($cart->get('mollie_payment_id'));
+        $order = Facades\Order::query()->where('mollie_payment_id', $payment->id)->first();
 
         $payment->isCancelable
             ? $this->mollie->payments->cancel($payment->id)
-            : $this->refund($cart->order(), $cart->order()->grandTotal());
+            : $this->refund($order, $order->grandTotal());
     }
 
     public function webhook(Request $request): Response
