bf11436dcce9eac5cf348dfcdef3cb4c85c2d806
Duncan McClean <duncan@duncanmcclean.com>
2026-04-07 09:34:35 +0100

    Fix amount off discount calculation when not perfectly divisible (#168)

--- tests/Discounts/Types/AmountOffTest.php
+++ tests/Discounts/Types/AmountOffTest.php
@@ -64,4 +64,30 @@ class AmountOffTest extends TestCase
         $this->assertEquals(75, $amountLine2);
         $this->assertEquals(100, $amountLine1 + $amountLine2);
     }
+
+    /**
+     * @see https://github.com/duncanmcclean/statamic-cargo/issues/164
+     */
+    #[Test]
+    public function it_distributes_fixed_amount_without_rounding_errors_when_not_perfectly_divisible()
+    {
+        // When 1000 (CHF 10) is divided by 3 equal line items, each gets 333.33...
+        // Using floor() would give 333 + 333 + 333 = 999, losing 1 cent
+        $discount = Discount::make()->type('amount_off')->set('amount_off', 1000);
+
+        $cart = Cart::make();
+        $cart->lineItems()->create(['id' => 'line1', 'total' => 3333]);
+        $cart->lineItems()->create(['id' => 'line2', 'total' => 3333]);
+        $cart->lineItems()->create(['id' => 'line3', 'total' => 3334]);
+
+        $discountType = (new AmountOff)->setDiscount($discount);
+
+        $amountLine1 = $discountType->calculate($cart, $cart->lineItems()->find('line1'));
+        $amountLine2 = $discountType->calculate($cart, $cart->lineItems()->find('line2'));
+        $amountLine3 = $discountType->calculate($cart, $cart->lineItems()->find('line3'));
+
+        // The total discount applied should equal the full discount amount (1000)
+        // not 999 due to rounding errors
+        $this->assertEquals(1000, $amountLine1 + $amountLine2 + $amountLine3);
+    }
 }
