a42b0cc821fcbe25d4c76a7d046a54c57d61ecaf
Duncan McClean <duncan@duncanmcclean.com>
2026-04-14 09:50:38 +0100

    add test to `DiscountQueryBuilderTest`

--- tests/Stache/Query/DiscountQueryBuilderTest.php
+++ tests/Stache/Query/DiscountQueryBuilderTest.php
@@ -12,6 +12,19 @@ class DiscountQueryBuilderTest extends TestCase
     use PreventsSavingStacheItemsToDisk;
 
     #[Test]
+    public function can_query_columns()
+    {
+        Discount::make()->handle('a')->type('percentage_off')->set('percentage_off', 10)->save();
+        Discount::make()->handle('b')->type('amount_off')->set('percentage_off', 1500)->save();
+        Discount::make()->handle('c')->type('percentage_off')->set('percentage_off', 15)->save();
+
+        $query = Discount::query()->where('type', 'percentage_off')->get();
+
+        $this->assertCount(2, $query);
+        $this->assertEquals(['a', 'c'], $query->map->id()->all());
+    }
+
+    #[Test]
     public function sorting_by_unsafe_method_does_not_invoke_it()
     {
         Discount::make()->handle('abc')->save();
