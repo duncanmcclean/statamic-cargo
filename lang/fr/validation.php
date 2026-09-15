04bde7096b2ccfff6fee8a2374e73807112a99ed
Florian Bouvot <florian@cometcie.fr>
2026-09-15 19:28:40 +0200

    Add French translations and register addon JSON language files

---
+++ lang/fr/validation.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'invalid_discount_code' => 'Le code de réduction est invalide.',
+    'no_physical_products' => 'Ce panier ne contient aucun produit physique.',
+    'prerequisite_product' => 'Vous devez acheter :product avant de pouvoir acheter ce produit.',
+    'prerequisite_product_logged_out' => 'Vous devez acheter un autre produit avant de pouvoir acheter celui-ci. Connectez-vous ou créez un compte pour continuer.',
+    'product_not_found' => 'Le produit est invalide.',
+    'product_out_of_stock' => 'Ce produit est actuellement en rupture de stock. Veuillez réessayer plus tard.',
+    'products_no_longer_available' => 'Un ou plusieurs articles de votre panier ne sont plus disponibles.',
+    'refund_greater_than_remaining' => 'Vous ne pouvez pas rembourser plus que le montant restant.',
+    'refund_greater_than_zero' => 'Vous devez saisir un montant supérieur à 0.',
+    'shipping_address_missing' => 'L’adresse de livraison est manquante dans le panier.',
+    'tax_class_invalid' => 'La catégorie de taxes sélectionnée est invalide.',
+    'unique_discount_value' => 'Cette valeur est déjà utilisée.',
+    'unique_tax_zone' => [
+        'countries' => 'Un ou plusieurs pays sont déjà associés à une autre zone de taxation.',
+        'postcodes' => 'Un ou plusieurs codes postaux sont déjà associés à une autre zone de taxation.',
+        'states' => 'Un ou plusieurs États sont déjà associés à une autre zone de taxation.',
+    ],
+    'variant_not_found' => 'La variante est invalide.',
+    'variant_out_of_stock' => 'Cette variante est actuellement en rupture de stock. Veuillez réessayer plus tard.',
+];
